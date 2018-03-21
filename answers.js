aeed3b1ad1339678baa02d6e5842c84d1651e2e7
vpdacoder <vaipatel@utexas.edu>
2018-03-21 16:41:25 -0500

    Commit 2 - Get Even answered

--- answers.js
+++ answers.js
@@ -5,3 +5,11 @@ console.log("I'm Working!");
 for (var i = 0; i < 21; i++) {
   console.log(i);
 }
+
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+for (var i = 0; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
