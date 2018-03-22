c3fc9b8ef72b01c6d0dcae471929c8c1f4486984
vpdacoder <vaipatel@utexas.edu>
2018-03-21 20:21:20 -0500

    Commit 4 - Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -34,3 +34,20 @@ for (var i = 0; i < 21; i++) {
   let a = Math.floor(Math.random()*(phrase.length));
   console.log(phrase[a]);
 }
+
+// FIZZBUZZ
+
+for (var i = 0; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Fizzbuzz");
+  }
+  else if (i % 3 == 0) {
+    console.log("fizz");
+  }
+  else if (i % 5 == 0) {
+    console.log("buzz");
+  }
+  else {
+    console.log(i);
+  }
+  }
